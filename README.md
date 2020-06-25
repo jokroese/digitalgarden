ee5a9e6f6da6021d96b21dab528054ca60859556
joekroese <joekroese@gmail.com>
2020-06-25 12:07:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Digital Garden
+
+An opinionated combination of [TiddlyWiki](tiddlywiki.com/) core and plugins.
+
+Uses the plugins:
+
+- [TiddlyMap](https://github.com/felixhayashi/TW5-TiddlyMap/)
+
+## Create digitalgarden.html
+
+1. Set up [TiddlyWiki with node.js](https://tiddlywiki.com/static/Installing%2520TiddlyWiki%2520on%2520Node.js.html)
+
+   1. Install [Node.js](https://tiddlywiki.com/static/Node.js.html).
+
+      - either from your favourite package manager: typically `apt-get install nodejs` on Debian/Ubuntu Linux or [Termux for Android](https://tiddlywiki.com/static/Serving%20TW5%20from%20Android.html), or `brew install node` on a Mac
+
+      - or directly from http://nodejs.org
+      
+   1. Install TiddlyWiki locally.
+
+        - Open a command line terminal and type: `npm install -g tiddlywiki`.
+
+        - If it fails with an error you may need to re-run the command as an administrator: `sudo npm install -g tiddlywiki` (Mac/Linux)
+
+   1. Check [TiddlyWiki](https://tiddlywiki.com/static/TiddlyWiki.html) is installed.
+      - Type `tiddlywiki --version`.
+
+1. Clone the [digitalgarden repository](https://github.com/joekroese/digitalgarden) or Download ZIP.
+
+1. Run `tiddlywiki . --rendertiddler $:/core/save/all digitalgarden.html text/plain` ([Reference](https://tiddlywiki.com/static/How%2520to%2520build%2520a%2520TiddlyWiki5%2520from%2520individual%2520tiddlers.html).)
+
+## Run digitalgarden through node.js
+
+1. Install node.js as in step 1. of 'Create digitalgarden.html'
+2. Create and run a server:
+   1. `tiddlywiki . --init server` to create a folder for a new wiki that includes server-related components
+   1. `tiddlywiki . --listen` to start
+   1. Visit http://127.0.0.1:8080/ in your browser
+   1. Try editing and creating tiddlers
---
+++ plugins/krystal/plugin.js
@@ -0,0 +1,248 @@
+/*\
+title: $:/plugins/rmnvsl/krystal/plugin.js
+type: application/javascript
+module-type: startup
+
+Sets plugin behavior
+
+\*/
+(function () {
+  const STORY_TIDDLER_TITLE = "$:/StoryList";
+  const ACTIVE_LINK_CLASS = "krystal-link--active";
+
+  exports.after = ["render"];
+
+  exports.startup = function () {
+    header();
+
+    window.addEventListener("resize", header);
+
+    const throttledTiddlerFrameEffects = throttle(tiddlerFrameEffects, 100);
+    window.addEventListener("scroll", throttledTiddlerFrameEffects, true);
+
+    $tw.rootWidget.addEventListener("tm-scroll", function (event) {
+      if (event.type === "tm-scroll") {
+        scroll(event.target);
+      }
+    });
+
+    $tw.hooks.addHook("th-navigating", closeTiddlersToRight);
+
+    $tw.wiki.addEventListener("change", highlightOpenTiddlerLinks);
+    $tw.wiki.addEventListener("change", reinitiateTiddlerFrameEffects);
+  };
+
+  function highlightOpenTiddlerLinks(changes) {
+    if (!$tw.utils.hop(changes, STORY_TIDDLER_TITLE)) {
+      return;
+    }
+
+    const config = $tw.wiki.getTiddler(
+      "$:/plugins/rmnvsl/krystal/config/highlight"
+    );
+
+    if (config && config.fields && config.fields.text === "disable") {
+      return;
+    }
+
+    const tiddlers = $tw.wiki.getTiddlerList(STORY_TIDDLER_TITLE);
+
+    Array.from(
+      document.querySelectorAll(`.${ACTIVE_LINK_CLASS}`)
+    ).forEach((link) => link.classList.remove(ACTIVE_LINK_CLASS));
+
+    tiddlers.forEach((tiddler) => {
+      Array.from(
+        document.querySelectorAll(
+          `.tc-tiddler-body a[href="#${encodeURIComponent(tiddler)}"]`
+        )
+      ).forEach((link) => link.classList.add(ACTIVE_LINK_CLASS));
+    });
+  }
+
+  function header() {
+    const height = document.querySelector(".krystal-header").offsetHeight;
+    document.documentElement.style.setProperty(
+      "--krystal-header-height",
+      `${height}px`
+    );
+  }
+
+  function scroll(tiddlerElement) {
+    const mediaQueryList = window.matchMedia("(min-width: 960px)");
+
+    if (mediaQueryList.matches) {
+      var storyRiver = tiddlerElement.parentElement;
+
+      var tiddlerPosition = Array.from(
+        storyRiver.querySelectorAll("div[data-tiddler-title]")
+      ).indexOf(tiddlerElement);
+      var tiddlerWidth = tiddlerElement.offsetWidth;
+      var windowWidth = window.innerWidth;
+
+      var position = windowWidth / 2 - tiddlerWidth / 2;
+      var newRiverPosition = Math.max(
+        tiddlerPosition * tiddlerWidth - position,
+        0
+      );
+
+      storyRiver.scroll({ left: newRiverPosition, behavior: "smooth" });
+    } else {
+      tiddlerElement.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  function tiddlerFrameEffects() {
+    var tiddlers = Array.from(document.querySelectorAll(".tc-tiddler-frame"));
+    var tiddlersCount = tiddlers.length;
+
+    if (tiddlersCount === 0) {
+      return;
+    }
+
+    var offset = 100;
+    var tiddlerPadding = 42;
+    var tiddlerWidth = tiddlers[0].offsetWidth;
+    var windowWidth = window.innerWidth;
+
+    tiddlers.forEach(function (tiddler, i) {
+      if (i === 0) {
+        return;
+      }
+
+      var tiddlerTitle = tiddler.querySelector(".krystal-tiddler__title");
+
+      // Edit Template
+      if (!tiddlerTitle) {
+        return;
+      }
+
+      var previousTiddler = tiddlers[i - 1];
+      var previousTiddlerTitle = previousTiddler.querySelector(
+        ".krystal-tiddler__title"
+      );
+
+      var x = tiddler.getBoundingClientRect().left;
+
+      var start = x < offset + i * tiddlerPadding;
+      var end =
+        x > windowWidth - (offset + (tiddlersCount - i) * tiddlerPadding);
+
+      var startOverlay = x < tiddlerWidth + (i - 1) * tiddlerPadding;
+
+      tiddler.classList.toggle("krystal-tiddler__frame--overlay", startOverlay);
+
+      if (previousTiddlerTitle) {
+        previousTiddlerTitle.classList.toggle(
+          "krystal-tiddler__title--start-active",
+          start
+        );
+
+        if (!end) {
+          previousTiddler.classList.toggle(
+            "krystal-tiddler__frame--hide",
+            start
+          );
+        }
+      }
+
+      if (start) {
+        tiddlerTitle.classList.remove("krystal-tiddler__title--end");
+      }
+
+      if (end) {
+        tiddler.classList.add("krystal-tiddler__frame--overlay");
+        tiddler.classList.add("krystal-tiddler__frame--hide");
+        tiddlerTitle.classList.add(
+          "krystal-tiddler__title--end",
+          "krystal-tiddler__title--end-active"
+        );
+      } else {
+        if (!startOverlay) {
+          tiddler.classList.remove("krystal-tiddler__frame--overlay");
+        }
+
+        tiddler.classList.remove("krystal-tiddler__frame--hide");
+        tiddlerTitle.classList.remove("krystal-tiddler__title--end-active");
+      }
+    });
+  }
+
+  function reinitiateTiddlerFrameEffects(changes) {
+    if (!$tw.utils.hop(changes, STORY_TIDDLER_TITLE)) {
+      return;
+    }
+    var duration = $tw.utils.getAnimationDuration() || 0;
+    setTimeout(tiddlerFrameEffects, duration);
+  }
+
+  function closeTiddlersToRight(event) {
+    const storyTiddler = $tw.wiki.getTiddler(STORY_TIDDLER_TITLE);
+    const tiddlers = $tw.wiki.getTiddlerList(STORY_TIDDLER_TITLE);
+
+    const navigateFrom = event.navigateFromTitle;
+    const navigateTo = event.navigateTo;
+
+    const config = $tw.wiki.getTiddler(
+      "$:/plugins/rmnvsl/krystal/config/close"
+    );
+
+    if (config && config.fields && config.fields.text === "disable") {
+      return event;
+    }
+
+    // Navigating from outside open tiddlers
+    if (!navigateFrom) {
+      return event;
+    }
+
+    // Destination tiddler already open
+    if (tiddlers.indexOf(navigateTo) === -1) {
+      const currentTiddlerIndex = tiddlers.indexOf(navigateFrom);
+      const tiddlersToClose = tiddlers.slice(currentTiddlerIndex + 1);
+
+      if (tiddlersToClose.length === 0) {
+        return event;
+      }
+
+      // Not working :(
+      // tiddlersToClose.forEach((tiddlerTitle) => () =>
+      //   $tw.rootWidget.dispatchEvent({
+      //     type: "tm-close-tiddler",
+      //     tiddlerTitle,
+      //   })
+      // );
+
+      const newStoryList = tiddlers.filter(
+        (title) => !tiddlersToClose.includes(title)
+      );
+
+      $tw.wiki.addTiddler(
+        new $tw.Tiddler({ title: STORY_TIDDLER_TITLE }, storyTiddler, {
+          list: newStoryList,
+        })
+      );
+    }
+
+    return event;
+  }
+
+  // ---
+
+  function throttle(callback, limit) {
+    var wait = false; // Initially, we're not waiting
+
+    return function () {
+      // We return a throttled function
+      if (!wait) {
+        // If we're not waiting
+        callback.call(); // Execute users function
+        wait = true; // Prevent future invocations
+        setTimeout(function () {
+          // After a period of time
+          wait = false; // And allow future invocations
+        }, limit);
+      }
+    };
+  }
+})();
